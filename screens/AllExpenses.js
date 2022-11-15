cd6918bd58055b5c1c16d558d7e701ac73b2eacf
ACHI9944 <achi9944@gmail.com>
2022-11-15 14:26:49 +0400

    Outputing Expenses, added button, iconbutton, added context for expenses and functions for changing expenses

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
+import { ExpensesContex } from "../store/expenses-context";
 
 function AllExpenses() {
-  return <Text>AllExpenses Screen</Text>;
+  const expensesCtx = useContext(ExpensesContex);
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found!"
+    />
+  );
 }
 
 export default AllExpenses;
